2d2b89c1185d864e6fa35a58fdcae9622083eca1
Dany Grimaldo <dany.grimaldo@gmail.com>
2020-03-21 22:43:09 -0400

    FinalDraft

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Establish Characters Used for Passwords Per Category
+var charU = "ABCDEFGHIJKFLMNOPQRSTUVWXYZ";
+var charL = "abcdefghijklmnopqrstuvwxyz";
+var symb = "!@#$^&_-*";
+var numb = "0123456789";
 
-  passwordText.value = password;
+//Set Data to Memory
+var charLength= document.getElementById("charLength")
 
-}
+var charUpper=document.getElementById("charU");
+var numBox= document.getElementById("numb");
+var symBox= document.getElementById("symb");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function() {
+
+(charLower.checked) ? characters += charL : "";
+(charUpper.checked) ? characters += charU : "";
+(symBox.checked) ? characters += symb : "";
+(numBox.checked) ? characters += numb : "";
+
+debugger;
+
+})
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = "";
+
+  //Create forLoop
+  for (var i=0; i<=length; i++) {
+    var password = password.charAt(Math.floor(Math.random() * Math.floor(password -1)));
+    password.push(password);
+
+  return password
+}
+}
